131cbe07b504309b97cd98ccee3a5c0f9ee47271
Alex Chikun <Claster85@gmail.com>
2020-10-04 19:37:47 +0300

    feat: add functions words1, words10, words100

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 module.exports = function toReadable (number) {
-  let arr1 = ['zero', 'one', 'two', 'three', 'four','five', 'six', 'seven', 'eight','nine',
-  'ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen',
-  'nineteen'];
-  let arr10 = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty',
-'ninety'];
-  let arr100 = ['hundred'];
+    let arr1 = ['zero', 'one', 'two', 'three', 'four','five', 'six', 'seven', 'eight',
+        'nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen',
+        'eighteen','nineteen'];
+    let arr10 = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty',
+        'ninety'];
+    let arr100 = ['hundred'];
 
-  function words1(num) {
-      return arr1[num];
-  };
-  function words10(num) {
-      return arr10[num]
-  };
-  function words100() {
-      return arr100[0];
-  }
+    function words1(num) {
+        return arr1[num];
+    };
+    function words10(num) {
+      let a = num.toString().slice(0, 1);
+      let b = num.toString().slice(1);
+      return (b === '0') ? `${arr10[a]}` : `${arr10[a]} ${arr1[b]}`;
+    };
+    function words100(num) {
+       let a = num.toString().slice(0, 1);
+       let b = num.toString().slice(1);
+       return `${words1(a)} ${arr100[0]} ${words10(b)}`;
+    }
 
-  if (number < 20) {
-    return words1(number);
-} else if (number < 100) {
-  let a = number.toString().slice(0, 1);
-  let b = number.toString().slice(1, 2);
-  return (b === '0') ? `${words10(a)}` : `${words10(a)} ${words1(b)}`;
-
-
-}
-
+    if (number < 20) {
+        return words1(number);
+    } else if (number < 100) {
+      return words10(number);
+    } else if (number < 1000) {
+      return words100(number)
+    }
 
 }
